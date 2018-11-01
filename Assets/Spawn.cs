49fe44ec8c3f0ee8e61491b237b11be71a495708
kaczy96 <kaczystudio@gmail.com>
2018-11-01 02:47:30 +0100

    Jumping, spawning moving obstacles and destroying them

---
+++ Assets/Spawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject obsctacle;
+    public float InstantiationTimer;
+    public float InstanttiationTimerStart;
+
+    void Start ()
+    {
+    }
+	
+	void Update ()
+    {
+        Create();
+    }
+
+    void Create()
+    {
+        InstantiationTimer -= Time.deltaTime;
+        if (InstantiationTimer <= 0)
+        {
+            Instantiate(obsctacle, transform.position+=transform.forward*Time.deltaTime, Quaternion.identity);
+            InstantiationTimer = InstanttiationTimerStart;
+        }
+
+    }
+}
