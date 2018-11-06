ff5185ede683686c9081f4fec12cfa11bcfb4ce0
kaczy96 <kaczystudio@gmail.com>
2018-11-06 16:22:27 +0100

    Manu 75% rdy

---
+++ Assets/GetScore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GetScore : MonoBehaviour
+{
+
+    public Text fscore;
+    float score = 0;
+
+    private void ShowScore()
+    {
+        Debug.Log(score);
+        score = Score.points;
+        fscore.text = score.ToString();
+    }
+    
+}
